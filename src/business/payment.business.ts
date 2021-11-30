37e921738009c5d8fddbae73f60db8da657a3d49
Bill <tvkhue96@gmail.com>
2021-12-01 02:02:34 +0700

    update payment business

--- src/business/payment.business.ts
+++ src/business/payment.business.ts
@@ -1,3 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+import { Expose } from 'class-transformer';
+
 export enum PAYMENT_STATUS {
   DECLINED = 'PS_DECLINED',
   CONFIRMED = 'PS_CONFIRMED',
@@ -7,3 +14,10 @@ export enum PAYMENT_REQUEST_ACTION {
   VERIFY = "ACTION_PAYMENT_VERIFY",
 }
 
+export class ProcessOrderPaymentDTO {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @Expose()
+  orderId!: string;
+}
